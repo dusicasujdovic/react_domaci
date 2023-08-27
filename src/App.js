27f00ac31ad164b362494a792bb697c4f872b749
dusicasujdovic <dusicasujdovic00@gmail.com>
2023-08-27 19:42:08 +0200

    Izmenjen App.js i App.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {Navbar} from "./components/navbar";
+import {Shop} from './pages/shop/Prodavnica'
+import {Cart} from './pages/cart/Korpa'
+import ONama from './pages/ONama'
+import { ProizvodiContextProvider } from './context/ProizvodiContext';
+import Footer from './components/footer';
+import Kontakt from './pages/Kontakt';
 import './App.css';
 
 function App() {
+
+  //const telefon = "+381 657890345";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ProizvodiContextProvider>
+              <Router>
+              <Navbar />
+              <Routes>
+                <Route path="/" element={<ONama />}/>
+                <Route path="/shop" element={<Shop/>} />
+                <Route path="/cart"  element={<Cart />} />
+                <Route path="/kontakt"  element={<Kontakt />} />
+              </Routes>
+              <Footer />
+              
+            
+              
+            </Router>
+      </ProizvodiContextProvider>
     </div>
-  );
+        );
 }
 
 export default App;
