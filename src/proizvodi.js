f5b97fef3bd9d1d13f62236b788c3c13e6c29df4
dusicasujdovic <dusicasujdovic00@gmail.com>
2023-08-27 18:42:36 +0200

    Kreirana lista proizvoda

---
+++ src/proizvodi.js
@@ -0,0 +1,88 @@
+import bb from'./images/bodybutter.PNG'
+import c from'./images/cream.PNG'
+import dc from'./images/daycream.PNG'
+import eg from'./images/eyegel.PNG'
+import f from'./images/foam.PNG'
+import lb from'./images/lipbalm.PNG'
+import l from'./images/lotion.PNG'
+import lc from'./images/lotioncactus.PNG'
+import m from'./images/mist.PNG'
+import nc from'./images/nightcream.PNG'
+import o from'./images/oil.PNG'
+import t from'./images/toner.PNG'
+
+export const PROIZVODI=[
+    {
+        id: 1,
+        naziv: "Ulje za lice",
+        cena: 900.00,
+        slika: o
+    },
+    {
+        id: 2,
+        naziv: "Pena za ciscenje lica",
+        cena: 1200.00,
+        slika: f
+    },
+    {
+        id: 3,
+        naziv: "Toner za ciscenje lica",
+        cena: 1400.00,
+        slika: t
+    },
+    {
+        id: 4,
+        naziv: "Dnevna krema",
+        cena: 2000.00,
+        slika: dc
+    },
+    {
+        id: 5,
+        naziv: "Dnevno-nocni fluid za lice",
+        cena: 1800.00,
+        slika: l
+    },
+    {
+        id: 6,
+        naziv: "Nocna krema",
+        cena: 2000.00,
+        slika: nc
+    },
+    {
+        id: 7,
+        naziv: "Skin food krema",
+        cena: 1200.00,
+        slika: c
+    },
+    {
+        id: 8,
+        naziv: "Skin Food puter za telo",
+        cena: 1700.00,
+        slika: bb
+    },
+    {
+        id: 9,
+        naziv: "Skin Food Lip Balm",
+        cena: 500.00,
+        slika: lb
+    },
+    {
+        id: 10,
+        naziv: "Mist za lice-Kaktus",
+        cena: 1700.00,
+        slika: m
+    },
+    {
+        id: 11,
+        naziv: "Hidratantni gel za zonu oko oka-Kaktus",
+        cena: 1000.00,
+        slika: eg
+    },
+    {
+        id: 12,
+        naziv: "Hidratantni fluid za lice-Kaktus",
+        cena: 1500.00,
+        slika: lc
+    },
+   
+]
